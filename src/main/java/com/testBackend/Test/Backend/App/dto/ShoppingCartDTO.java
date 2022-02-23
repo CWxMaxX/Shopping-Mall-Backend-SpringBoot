e0d3c82a2e22dc7b62252b00e666f4b845e8cbbd
Chamith Wijesooriya <s16538@sci.pdn.ac.lk>
2022-02-23 11:49:38 +0530

    Created Branch 1

--- src/main/java/com/testBackend/Test/Backend/App/dto/ShoppingCartDTO.java
+++ src/main/java/com/testBackend/Test/Backend/App/dto/ShoppingCartDTO.java
@@ -1,4 +1,19 @@
 package com.testBackend.Test.Backend.App.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class ShoppingCartDTO {
+    private String cartID;
+
+    private String uID;
+    private String deliveryStatus;
+    private String deliveryAddress;
+    private String paymentStatus;
 }
